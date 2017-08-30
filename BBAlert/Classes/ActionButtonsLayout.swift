350c63475e69f13f86ff99ade6dc92132adebbee
Pavel Kotenko <stanleeforyou@gmail.com>
2017-08-30 12:23:08 +0600

    stack view in actionsLayout instead of constraint layouting

--- BBAlert/Classes/ActionButtonsLayout.swift
+++ BBAlert/Classes/ActionButtonsLayout.swift
@@ -10,7 +10,7 @@ import Foundation
 import SnapKit
 
 // Protocol for any button in alerts
-public protocol AlertPressable {
+public protocol AlertPressable: AnyObject {
     var pressBlock: (() -> Void)? { get set }
 }
 
@@ -28,38 +28,17 @@ open class VerticalActionsLayout: ActionsLayout {
     }
     
     public func configureView(inContainer container: UIView) -> UIView? {
-        guard let buttons = alertButtons else {
+        guard let buttons = alertButtons as? [UIView] else {
             return nil
         }
         
         var frame = CGRect(origin: .zero, size: container.frame.size)
-        let wrapperView = UIView(frame: frame)
-        
-        var i = 0
-        var resultHeight: CGFloat = 0
-        for actionButton in buttons {
-            guard let actionButton = actionButton as? UIView else {
-                continue
-            }
-            
-            let buttonFrame = CGRect(x: 0, y: CGFloat(i) * actionButton.frame.height, width: frame.width, height: actionButton.frame.height)
-            actionButton.frame = buttonFrame
-            wrapperView.addSubview(actionButton)
-            
-            actionButton.snp.makeConstraints { (make) in
-                make.top.equalTo(CGFloat(i) * actionButton.frame.height)
-                make.left.equalTo(0)
-                make.right.equalTo(0)
-                make.height.equalTo(actionButton.frame.height)
-            }
-            
-            resultHeight += buttonFrame.height
-            i += 1
-        }
-        
+        let wrapperView = UIStackView(arrangedSubviews: buttons)
+        wrapperView.spacing = 0.5
+        wrapperView.axis = .vertical
+
         wrapperView.snp.makeConstraints { (make) in
             make.width.equalTo(container.frame.width)
-            make.height.equalTo(resultHeight)
         }
         
         wrapperView.layoutIfNeeded()
